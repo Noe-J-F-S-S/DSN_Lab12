d4ddbcca49f40b00b05a658fd3aa945cd6e02e18
Noe <nodaxdx@gmail.com>
2022-11-12 13:10:18 -0500

    primer commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const myconnection = require('express-myconnection')
+const bodyParser = require('express-myconnection')
+const mysql = require('mysql')
+//const tasksRoutes = require('./routes/tasks')
+const app = express();
+app.set('port',3000)
+
+app.set('views', __dirname +'/views')
+app.engine('.hbs', engine({
+    extname:'.hbs'
+}))
+app.set('view engine', 'hbs');
+app.use(myconnection(mysql,{
+    host: 'localhost',
+    user: 'root',
+    password:'',
+    port: 3306,
+    database:'crudcontactos'
+}))
+
+
+app.listen(app.get('port'),() =>{
+    console.log('Listen on port ', app.get('port'))
+})
+
+app.get('/',(req,res) => {
+    res.render('home')
+})
